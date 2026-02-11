a12f8fe59e0d82d51a74ce7c0a79db3ddfa27575
Студенческая <@rbiu.local>
2026-02-11 11:12:26 +0500

    dark theme

--- js/script.js
+++ js/script.js
@@ -1,6 +1,49 @@
 const cards = document.querySelectorAll('.card'); //выбираем  все элементы на странице с классом card на странице
-for (let card of cards){
-    card.addEventListener("click", ()=>{
-    card.classList.toggle("card-show");
-});
+let openCards = [];
+let card1
+let count = 0;
+const text = document.querySelector(".count");
+text.textContent = "Количество ходов:" + count
+
+for (let card of cards) {
+    card.addEventListener("click", () => {
+        let pathImg = card.querySelector('img').src;
+        if (openCards.length == 0) { //если массив пустой
+            card1 = card;
+            openCards.push(pathImg);
+            card.classList.add("card-show");
+        }
+        else if (openCards.length == 1) {
+            count++;
+            text.textContent = "Количество ходов:" + count
+            openCards.push(pathImg);
+            card.classList.add("card-show");
+
+            if (openCards[0] == openCards[1]) {
+                setTimeout(function () {
+                    card.classList.add("card-finish");
+                    card1.classList.add("card-finish")
+                }, 3000);
+
+            } else {
+                setTimeout(function () {
+                    card.classList.remove("card-show");
+                    card1.classList.remove("card-show");
+                }, 3000);
+            }
+            openCards = []
+        }
+        console.log(openCards);
+    });
 }
+
+
+// const h1 = document.querySelector('h1');
+// setTimeout(function(){
+//     h1.style.color = "red";
+// }, 5000)
+// let i = 0;
+// setInterval(function(){
+//     i++;
+//     h1.innerHTML= i;
+// }, 1000);
